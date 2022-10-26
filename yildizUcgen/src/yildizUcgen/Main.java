f8f999ec9761dd8ce7aaf9a5f7796d7e6fc23543
MBilcanoglu <72490880+MBilcanoglu@users.noreply.github.com>
2022-10-26 23:36:59 +0300

    Add files via upload

---
+++ yildizUcgen/src/yildizUcgen/Main.java
@@ -0,0 +1,27 @@
+package yildizUcgen;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int n;
+		
+		Scanner scan=new Scanner(System.in);
+		
+		System.out.print("Bir sayý giriniz: ");
+		n=scan.nextInt();
+		
+		for(int i=0; i<=n; i++) {
+			for(int k=0; k<(n-i); k++) {
+				System.out.print(" ");
+			}
+			for(int j=1; j<=(2*i+1); j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
